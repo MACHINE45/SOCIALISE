d3ed4cd0df36341b63a782ba103c9bcafceb83cb
VINEET KUMAR CHAUBEY <93907642+MACHINE45@users.noreply.github.com>
2023-07-15 16:27:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,3 +10,14 @@ import path from "path";
 import  { fileURLToPath } from "url";
 
 /* CONFIGURATION */
+const  __filename = fileURLToPath (import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express.json();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({ limit : "30mb,extended: true "}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
